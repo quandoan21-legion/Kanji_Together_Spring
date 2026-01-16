7c6dc9d6ed7c8a967b4df312fa6b5dfde12bbc99
Babyincar0311 <phamgiakhang0311@gmail.com>
2026-01-16 21:06:01 +0700

    test

--- src/main/java/org/t2404e/kanji_together_db/entity/KanjiLessons.java
+++ src/main/java/org/t2404e/kanji_together_db/entity/KanjiLessons.java
@@ -16,7 +16,7 @@ public class KanjiLessons {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Nên để private
 
-    // Map với cột tên bài học trong DB (ví dụ: name)
+    // Map với cột tên bài học trong DB
     @Column(name = "name")
     private String name;
 
